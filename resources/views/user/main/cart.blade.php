8d2409473ae4104865638111d99c4b81c4e40c54
Hein-Thant-Zin <heintz414@gmail.com>
2023-02-22 15:43:52 +0630

    cart complete

--- resources/views/user/main/cart.blade.php
+++ resources/views/user/main/cart.blade.php
@@ -33,48 +33,46 @@
                                             </button>
                                         </div>
                                         <input type="text" value="{{ $c->qty }}"
-                                            class="form-control form-control-sm bg-secondary border-0 text-center" ">
-                                                                                                                                                                            <div class="input-group-btn">
-                                                                                                                                                                                <button class="btn btn-sm btn-primary btn-plus">
-                                                                                                                                                                                    <i class="fa fa-plus"></i>
-                                                                                                                                                                                </button>
-                                                                                                                                                                            </div>
-                                                                                                                                                                        </div>
-                                                                                                                                                                    </td>
-                                                                                                                                                                    <td class="align-middle">$150</td>
-                                                                                                                                                                    <td class="align-middle"><button class="btn btn-sm btn-danger"><i
-                                                                                                                                                                                class="fa fa-times"></i></button></td>
-                                                                                                                                                                </tr>
-     @endforeach
-
-                    </tbody>
-                </table>
+                                            class="form-control form-control-sm bg-secondary border-0 text-center>                                                                                                                                                                      <div class="input-group-btn">
+                                        <button class="btn btn-sm btn-primary btn-plus">
+                                            <i class="fa fa-plus"></i>
+                                        </button>
+                                    </div>
             </div>
-            <div class="col-lg-4">
-                <h5 class="section-title position-relative text-uppercase mb-3"><span class="bg-secondary pr-3">Cart
-                        Summary</span></h5>
-                <div class="bg-light p-30 mb-5">
-                    <div class="border-bottom pb-2">
-                        <div class="d-flex justify-content-between mb-3">
-                            <h6>Subtotal</h6>
-                            <h6>$150</h6>
-                        </div>
-                        <div class="d-flex justify-content-between">
-                            <h6 class="font-weight-medium">Shipping</h6>
-                            <h6 class="font-weight-medium">$10</h6>
-                        </div>
+            </td>
+            <td class="align-middle">{{ $c->pizza_price * $c->qty }} Ks</td>
+            <td class="align-middle"><button class="btn btn-sm btn-danger"><i class="fa fa-times"></i></button></td>
+            </tr>
+            @endforeach
+
+            </tbody>
+            </table>
+        </div>
+        <div class="col-lg-4">
+            <h5 class="section-title position-relative text-uppercase mb-3"><span class="bg-secondary pr-3">Cart
+                    Summary</span></h5>
+            <div class="bg-light p-30 mb-5">
+                <div class="border-bottom pb-2">
+                    <div class="d-flex justify-content-between mb-3">
+                        <h6>Subtotal</h6>
+                        <h6>$150</h6>
                     </div>
-                    <div class="pt-2">
-                        <div class="d-flex justify-content-between mt-2">
-                            <h5>Total</h5>
-                            <h5>$160</h5>
-                        </div>
-                        <button class="btn btn-block btn-primary font-weight-bold my-3 py-3">Proceed To
-                            Checkout</button>
+                    <div class="d-flex justify-content-between">
+                        <h6 class="font-weight-medium">Shipping</h6>
+                        <h6 class="font-weight-medium">$10</h6>
                     </div>
+                </div>
+                <div class="pt-2">
+                    <div class="d-flex justify-content-between mt-2">
+                        <h5>Total</h5>
+                        <h5>$160</h5>
+                    </div>
+                    <button class="btn btn-block btn-primary font-weight-bold my-3 py-3">Proceed To
+                        Checkout</button>
                 </div>
             </div>
         </div>
+    </div>
     </div>
     <!-- Cart End -->
 @endsection
